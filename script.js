0cb6bad93a0b0e3b08d00d9d33ba3c1430591066
Dhruv Sood <dhruvsoodboss@gmail.com>
2023-03-03 21:56:57 +0530

    Linked with js support #2

---
+++ script.js
@@ -0,0 +1,4 @@
+const pr1btn = document.querySelector('#restaurant');
+    pr1btn.addEventListener('click',()=>{
+        window.open("https://dhruv-sood.github.io/front-end-projects/Restaurant%20Website/restraunt_website.html", "_blank")
+    })
