0ba21e44d998117e2bc54f32c282ffad135140c2
Dhruv Sood <dhruvsoodboss@gmail.com>
2023-03-05 13:48:26 +0530

    Added coming soon template project

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const pr1btn = document.querySelector('#restaurant');
-    pr1btn.addEventListener('click',()=>{
-        window.open("https://dhruv-sood.github.io/front-end-projects/Restaurant%20Website/restaurant_website.html", "_blank")
-    })
+pr1btn.addEventListener('click', () => {
+    window.open("https://dhruv-sood.github.io/front-end-projects/Restaurant Website/restaurant_website.html", "_blank")
+})
+
+const pr2btn = document.querySelector('#coming_soon');
+pr2btn.addEventListener('click', () => {
+    window.open("https://dhruv-sood.github.io/front-end-projects/Coming Soon Template/coming_soon.html", "_blank")
+})
