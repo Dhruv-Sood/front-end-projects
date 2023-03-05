d35a2d75ec5b392bc0057eb96259e17ab9fee533
Dhruv Sood <dhruvsoodboss@gmail.com>
2023-03-05 21:29:30 +0530

    Added Parallax Website

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ const pr2btn = document.querySelector('#coming_soon');
 pr2btn.addEventListener('click', () => {
     window.open("https://dhruv-sood.github.io/front-end-projects/Coming Soon Template/coming_soon.html", "_blank")
 })
+
+const pr3btn = document.querySelector('#parallax');
+pr3btn.addEventListener('click', () => {
+    window.open("https://dhruv-sood.github.io/front-end-projects/Parallax Website/parallax_website.html", "_blank")
+})
